46c0d4ecc38314e484955df977bfe58d102f6d5a
ZholudZI <113444940+ZholudZI@users.noreply.github.com>
2025-07-23 20:44:05 +0300

    lab2

--- lab2/Program.cs
+++ lab2/Program.cs
@@ -1,11 +1,27 @@
-﻿interface IFighter
+﻿abstract class Stats
+{
+	int health = 0;
+	int strength = 0;
+}
+
+class Race : Stats
+{
+
+}
+class Class : Stats
+{
+
+}
+
+class Character : IFighter
+{ 
+	
+}
+
+interface IFighter
 {
 	void Attack();
 	void CheckStats();
-}
-class Character
-{
-
 }
 
 //Dictionary<string, Character> characters = new Dictionary<string, Character>();
