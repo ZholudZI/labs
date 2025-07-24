9b8406519cc002436625a0046550debc7e38ee91
ZholudZI <113444940+ZholudZI@users.noreply.github.com>
2025-07-25 00:22:12 +0300

    lab2-1

---
+++ lab2-1/Program.cs
@@ -0,0 +1,27 @@
+﻿abstract class Stats
+{
+	int health = 0;
+	int strength = 0;
+}
+
+class Race : Stats
+{
+
+}
+class Class : Stats
+{
+
+}
+
+class Character : IFighter
+{
+
+}
+
+interface IFighter
+{
+	void Attack();
+	void CheckStats();
+}
+
+//Dictionary<string, Character> characters = new Dictionary<string, Character>();
