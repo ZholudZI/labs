f9c8e111653a0827842318ec4e5c75d80d6445ea
ZholudZI <113444940+ZholudZI@users.noreply.github.com>
2025-07-30 18:36:28 +0300

    lab1-2

--- lab1-2/Program.cs
+++ lab1-2/Program.cs
@@ -1,46 +1,36 @@
-﻿void bubbleSort(List<int> list)
+﻿class Program
 {
-	for (int j = 0; j < list.Count - 1; j++)
-	{
-		for (int i = 0; i < list.Count - 1; i++)
-		{
-			if (list[i] > list[i + 1])
-			{
-				int temp = list[i];
-				list[i] = list[i + 1];
-				list[i + 1] = temp;
-			}
-		}
-	}
-}
+    static void Main( string[] args )
+    {
+        Program program = new Program();
+        List<int> numbers = new List<int>() { 4, 3, 2, 1 };
+        program.OptimizeBubbleSort( numbers );
+        program.PrintList( numbers );
+    }
+    private void OptimizeBubbleSort( List<int> numbers )
+    {
+        bool isSorted = false;
+        for ( int j = 0; j < numbers.Count - 1 && !isSorted; j++ )
+        {
+            isSorted = true;
+            for ( int i = 0; i < numbers.Count - 1; i++ )
+            {
+                if ( numbers[ i ] > numbers[ i + 1 ] )
+                {
+                    isSorted = false;
+                    int temp = numbers[ i ];
+                    numbers[ i ] = numbers[ i + 1 ];
+                    numbers[ i + 1 ] = temp;
+                }
+            }
+        }
+    }
+    private void PrintList( List<int> list )
+    {
+        foreach ( int el in list )
+        {
+            Console.Write( $"{el}; " );
+        }
+    }
 
-void optimizeBubbleSort(List<int> list)
-{
-	bool isSorted = false;
-	for (int j = 0; j < list.Count - 1 && !isSorted; j++)
-	{
-		isSorted = true;
-		for (int i = 0; i < list.Count - 1; i++)
-		{
-			if (list[i] > list[i + 1])
-			{
-				isSorted = false;
-				int temp = list[i];
-				list[i] = list[i + 1];
-				list[i + 1] = temp;
-			}
-		}
-	}
 }
-
-void printList(List<int> list)
-{
-	foreach (int el in list)
-	{
-		Console.Write($" {el};");
-	}
-}
-
-List<int> numbers = new List<int>() { 4, 3, 2, 1 };
-optimizeBubbleSort(numbers);
-printList(numbers);
